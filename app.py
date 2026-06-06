2fde041519cd1ede6f6b087d52a1a013d754a5b7
dinesh ajmera <mynamedineshajmera@gmail.com>
2026-06-06 11:29:35 +0530

    database is connected and login is success fully done

--- app.py
+++ app.py
@@ -1,10 +1,55 @@
-from flask import Flask , render_template , request 
+from flask import Flask, render_template, request
+from models import db, User
 
 app = Flask(__name__)
-@app.route("/")
-def enter():
-    return render_template("login.html")
+
+app.config['SECRET_KEY'] = 'vendorbridge123'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///vendorbridge.db'
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+
+    if request.method == 'POST':
+
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        role = request.form['role']
+
+        user = User(
+            name=name,
+            email=email,
+            password=password,
+            role=role
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        return "User Registered Successfully"
+
+    return render_template('signup.html')
+
+
+@app.route('/users')
+def users():
+
+    all_users = User.query.all()
+
+    result = ""
+
+    for user in all_users:
+        result += f"{user.name} | {user.email} | {user.role}<br>"
+
+    return result
+
 
 
 if __name__ == "__main__":
-    app.run(debug=True , port=5000)
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,68 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+
+db = SQLAlchemy()
+
+
+class Vendor(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    company_name = db.Column(db.String(200))
+
+    email = db.Column(db.String(100))
+
+    phone = db.Column(db.String(20))
+
+    gst_number = db.Column(db.String(50))
+
+    category = db.Column(db.String(100))
+
+    status = db.Column(db.String(50))
+
+class RFQ(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    title = db.Column(db.String(200))
+
+    description = db.Column(db.Text)
+
+    quantity = db.Column(db.Integer)
+
+    deadline = db.Column(db.Date)
+
+    status = db.Column(db.String(50))
+
+    created_by = db.Column(db.Integer)
+
+class Quotation(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    rfq_id = db.Column(db.Integer)
+
+    vendor_id = db.Column(db.Integer)
+
+    price = db.Column(db.Float)
+
+    delivery_days = db.Column(db.Integer)
+
+    notes = db.Column(db.Text)
+
+    status = db.Column(db.String(50))
+
+
+
+class User(UserMixin, db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(100))
+
+    email = db.Column(db.String(100), unique=True)
+
+    password = db.Column(db.String(200))
+
+    role = db.Column(db.String(50))
